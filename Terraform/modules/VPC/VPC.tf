7e65dd546280f999b24c8ebfa6c1ae2f039bb12a
universe <davidrolandoaguirre@gmail.com>
2024-05-15 12:17:15 -0500

    fix git

--- Terraform/modules/VPC/VPC.tf
+++ Terraform/modules/VPC/VPC.tf
@@ -31,7 +31,7 @@ resource "aws_route_table" "public_route_table" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = data.aws_internet_gateway.igw
+    gateway_id = data.aws_internet_gateway.igw.id
   }
 }
 
