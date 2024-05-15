7a56a562158ad9696a233543fd657df228e2bd74
universe <davidrolandoaguirre@gmail.com>
2024-05-15 11:13:40 -0500

    block egress

--- Terraform/modules/VPC/VPC.tf
+++ Terraform/modules/VPC/VPC.tf
@@ -74,10 +74,11 @@ resource "aws_security_group" "jenkins_security_group" {
     }
   ]
 
-  egress = {
+  egress = [{
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-  }
+    }
+  ]
 }
